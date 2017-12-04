1c5be2a1b5d56a46e6b756c20ddc7d188a37a3fe
Ryan <ryan.ross@temple.edu>
2017-12-03 23:19:19 -0500

    Integrate twilio and google maps

--- sms-maps.py
+++ sms-maps.py
@@ -1,15 +1,23 @@
 from twilio.rest import Client
-import os
-# Your Account SID from twilio.com/console
-account_sid = os.environ['TWILIO_SID']
-# Your Auth Token from twilio.com/console
-auth_token  = os.environ['TWILIO_TOKEN']
+import os, googlemaps
 
-client = Client(account_sid, auth_token)
+twilio_account_sid = os.environ['TWILIO_SID']
+twilio_auth_token  = os.environ['TWILIO_TOKEN']
+gmaps_api_token = os.environ['GMAPS_DISTANCE_API_KEY']
 
-message = client.messages.create(
-    to="+12159832573",
-    from_="+18564315999 ",
-    body="Hi test!")
+gmaps = googlemaps.Client(key=gmaps_api_token)
+twilClient = Client(twilio_account_sid, twilio_auth_token)
 
-print(message.sid)
+def getDestinationResult(startDestination, endDestination):
+    results = gmaps.distance_matrix(startDestination, endDestination, mode="driving",units="imperial")
+    return results
+
+def sendText(message):
+    textMessage = twilClient.messages.create(
+        to="+12159832573",
+        from_="+18564315999",
+        body=message)
+
+if __name__ == '__main__':
+    gmapResults = getDestinationResult('517 Stratford Road, Glenolden, PA','1741 Fontain Street, Philadelphia, PA')
+    sendText("From " + gmapResults['origin_addresses'][0] + " to " + gmapResults['destination_addresses'][0] + " will take you " + gmapResults['rows'][0]['elements'][0]['duration']['text'] + " to drive there!")
