2ed6f1a1b7ada93da716b925af169e147a2117f4
Ryan <ryan.ross@temple.edu>
2017-12-17 17:09:24 -0500

    Changed code to account for AWS python libraries. Parsed body

--- sms-maps.py
+++ sms-maps.py
@@ -1,40 +1,31 @@
-from twilio.rest import Client
+from __future__ import print_function
 import os
-import googlemaps
+import json
 
-twilio_account_sid = os.environ['TWILIO_SID']
-twilio_auth_token  = os.environ['TWILIO_TOKEN']
 gmaps_api_token = os.environ['GMAPS_DISTANCE_API_KEY']
+gmaps_url_base = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+parsed_message = {"start_destination":"", "end_destination":""}
 
-gmaps = googlemaps.Client(key=gmaps_api_token)
-twil_client = Client(twilio_account_sid, twilio_auth_token)
+def format_message(event_body):
+    message = event_body
+    message = message.replace("+", " ")
+    message = message.replace("%7C", "|")
+    message = message.replace("%2C", ",")
+    return message
 
-def get_destination_result(start_destination, end_destination):
-    results = gmaps.distance_matrix(start_destination, end_destination, mode="walking",units="imperial")
-    return results
+def parse_message(event):
+    event_body_formated = format_message(event['Body'])
+    print(event_body_formated)
+    event_body_split = event_body_formated.split("|")
 
-def get_start_destination(gmap_results):
-    return gmap_results['origin_addresses'][0]
+    parsed_message['start_destination'] = event_body_split[0]
+    parsed_message['end_destination'] = event_body_split[1]
+    print(parsed_message)
 
-def get_end_destination(gmap_results):
-    return gmap_results['destination_addresses'][0]
 
-def get_trip_duration(gmap_results):
-    return gmap_results['rows'][0]['elements'][0]['duration']['text']
+def lambda_handler(event, context):
+    print("Received event: " + str(event))
+    parse_message(event)
 
-def get_gmap_status(gmap_results):
-    return gmap_results['status']
-
-def send_text(message):
-    sms_message = twil_client.messages.create(
-        to="+12159832573",
-        from_="+18564315999",
-        body=message)
-
-if __name__ == '__main__':
-    gmap_results = get_destination_result('517 Stratford Road, Glenolden, PA','1741 Fontain Street, Philadelphia, PA')
-    import pdb; pdb.set_trace()
-    send_text("From " + get_start_destination(gmap_results) +
-             " to " + get_end_destination(gmap_results) +
-             " will take you " + get_trip_duration(gmap_results) +
-             " to drive there!")
+    return '<?xml version=\"1.0\" encoding=\"UTF-8\"?>'\
+          '<Response><Message>From: '+ str(parsed_message['start_destination']) + ' To: ' + str(parsed_message['end_destination']) +'</Message></Response>'
